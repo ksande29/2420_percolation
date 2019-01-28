7d24362c0171a6cff35dcff9421146264e78d954
Kate <ke.sanders4404@gmail.com>
2019-01-28 16:37:49 -0700

    Added Method Comments
    Added comments

--- Percolation.java
+++ Percolation.java
@@ -2,14 +2,29 @@ package percolation;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+/**
+ * 
+ * @author Kate
+ *
+ */
 public class Percolation 
 {
-	private int N;
+	private int N; 						//number of rows/columns
 	private boolean[][] isSqrOpen; 
 	private WeightedQuickUnionUF uf;
-	private int top;
-	private int bottom;
+	private int top; 					//index of top node in WeightedQuickUnion
+	private int bottom; 				//index of bottom node in WeightedQuickUnion
 	
+	/**
+	 * Constructor 
+	 * Throws an error if a value less than 1 is entered
+	 * Creates a 2D boolean array (which corresponds to each square in the percolation grid) and sets
+	 * all values to 0 in the array.
+	 * Creates an instance of WeightedUnionFind (to be able to connect open squares) and connects all
+	 * of the squares on the top to a virtual node called top and all squares on the bottom row to a 
+	 * virtual node called bottom.
+	 * @param N (int) number of rows/columns
+	 */
 	public Percolation(int N) // create N­by­N grid, with all sites blocked
 	{		
 		if (N <= 0)	 
@@ -47,6 +62,11 @@ public class Percolation
 		}
 	}
 	
+	/**
+	 * Opens a specified square and connects that open square to any open squares that are next to it
+	 * @param i (int) row number
+	 * @param j (int) column number
+	 */
 	public void open(int i, int j) // open site (row i, column j) if it is not open already
 	{
 		if( (i < 0 || i > N-1) || (j < 0 || j > N-1) )
@@ -73,14 +93,26 @@ public class Percolation
 		}
 	}
 	
+	/**
+	 * Converts the position of a square in the 2D array to the corresponding index in a 1D array
+	 * (which can be used with WeightedUnionFind)
+	 * Indexes start at 1 because the index 0 is reserved for the top node
+	 * @param i (int) column number
+	 * @param j (int) row number
+	 * @return
+	 */
 	private int getFlatIndex(int i, int j) 
 	{
-		//convert the row and column number to a 1D array index that can be used with uf
-		//0 represents the top node
 		int index = i*N + j + 1;
 		return index;
 	}
 	
+	/**
+	 * Checks if a square is open
+	 * @param i (int) - row number
+	 * @param j (int) - column number
+	 * @return
+	 */
 	public boolean isOpen(int i, int j) // is site (row i, column j) open?
 	{
 		if( (i < 0 || i > N-1) || (j < 0 || j > N-1) )
@@ -93,6 +125,12 @@ public class Percolation
 		}
 	}
 	
+	/**
+	 * Checks if there is water in an square
+	 * @param i (int) - row number
+	 * @param j (int) - column number
+	 * @return
+	 */
 	public boolean isFull(int i, int j) // is site (row i, column j) full?
 	{
 		if( (i < 0 || i > N-1) || (j < 0 || j > N-1) )
@@ -107,13 +145,16 @@ public class Percolation
 			return false;
 		}
 	}
-		
+	
+	/**
+	 * Checks if the top node is connected to the bottom node (checks if water can reach from the
+	 * top to the bottom)
+	 * @return
+	 */
 	public boolean percolates() // does the system percolate? 
 	{				
 		//if top connected to bottom
-		if (uf.connected(top, bottom))
-			return true;
-		return false;
+		return uf.connected(top, bottom);
 	}
 
 }
--- PercolationStats.java
+++ PercolationStats.java
@@ -3,6 +3,11 @@ package percolation;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
+/**
+ * 
+ * @author Kate
+ *
+ */
 public class PercolationStats 
 {
 	private Percolation percolation;
@@ -11,6 +16,14 @@ public class PercolationStats
 	private int x;
 	private int y;
 		
+	/**
+	 * Constructor
+	 * Performs a specified number of tests. Each test creates an instance of a percolation (with no
+	 * open sites) and then randomly opens sites one at a time until the instance percolates. Based off 
+	 * of the results, the percolation threshold for each instance is calculated.
+	 * @param N (int) - number of rows/columns
+	 * @param T (int) - number of times to repeat the test
+	 */
 	public PercolationStats(int N, int T) // perform T independent experiments on an N­by­N grid
 	{
 		if (N <= 0 || T <= 0)	 
@@ -30,50 +43,95 @@ public class PercolationStats
 		        {
 		           	x = StdRandom.uniform(N);
 		            y = StdRandom.uniform(N);
-		            //System.out.println("x: " + x);
-	                //System.out.println("y: " + y);
+		            //System.out.println("X: " + x + " Y: " + y);
 		            if (!percolation.isOpen(x, y)) 
 		            {
 		               	percolation.open(x, y);
 		                numOpen++;
 		            }
 		        }
-		        thresholds[i] = (double) numOpen / (N*N);   
+		        
+		        thresholds[i] = (double) numOpen / (N*N); 
+		        
+		        //Used for testing
+		        //System.out.println("number open: " + numOpen);
+		        //System.out.println("total number: " + N*N);
+		        //double threshold = (double) numOpen / (N*N);
+		        //System.out.println("threshold: " + threshold);
+		        //System.out.println();  
 			}
-			
-			System.out.printf("mean: %.2f%n", mean());
-			System.out.printf("standard deviation: %.2f%n", stddev());
-			System.out.printf("95%% confident that the threshold is between %.2f and %.2f%n", confidenceLow(), confidenceHigh());
 		}
 	}
-		
+	
+	/**
+	 * Calculates the mean of the values in an array of doubles
+	 * @return mean (double)
+	 */
 	public double mean() // sample mean of percolation threshold
 	{
 		double mean = StdStats.mean(thresholds);
 		return mean;
 	}
-		
+	
+	/**
+	 * Calculates the standard deviation of the values in an array of doubles
+	 * @return standard deviation (double)
+	 */
 	public double stddev() // sample standard deviation of percolation threshold
 	{
 		double stddev = StdStats.stddev(thresholds);
 		return stddev;
 	}
 		
+	/**
+	 * Calculates the the lower bound in a 95% confidence interval from the values in
+	 * an array of doubles
+	 * @return low end of 95% confidence interval (double)
+	 */
 	public double confidenceLow() // low endpoint of 95% confidence interval
 	{
 		return mean() - stddev();
 	}
-		
+	
+	/**
+	 * Calculates the the upper bound in a 95% confidence interval from the values in
+	 * an array of doubles
+	 * @return high end of 95% confidence interval (double)
+	 */
 	public double confidenceHigh() // high endpoint of 95% confidence interval
 	{
 		return mean() + stddev();
 	}
 
+	/**
+	 * Prints out the mean, standard deviation, and confidence interval
+	 */
+	private void printResults()
+	{		
+		System.out.printf("mean: %.2f%n", mean());
+		System.out.printf("standard deviation: %.2f%n", stddev());
+		System.out.printf("95%% confident that the threshold is between %.2f and %.2f%n", confidenceLow(), confidenceHigh());
+	}
 	
+	/**
+	 * Test Client - tests the PercolationStats class
+	 * @param args
+	 */
 	public static void main(String[] args)
 	{
-		PercolationStats stats = new PercolationStats(5, 100);
+		PercolationStats stats = new PercolationStats(3, 1000);
+		stats.printResults();
+		System.out.println();
+		
+		PercolationStats stats2 = new PercolationStats(4, 1000);
+		stats2.printResults();
+		System.out.println();
+		
+		PercolationStats stats3 = new PercolationStats(5, 1000);
+		stats3.printResults();
+		System.out.println();
 	}
+	
 
 }
 
